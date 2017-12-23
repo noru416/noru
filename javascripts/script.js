3cf8b0441b242aaadc98d6bfb7d2f72703d69fc4
R nD <rnd@Rui-MacBook-Pro.local>
2017-12-23 22:07:12 +0900

    debugging

--- javascripts/script.js
+++ javascripts/script.js
@@ -23,6 +23,7 @@ $(document).on('click', '.navigation-link',function() {
 $(document).on('click', '.spectrum-item',function() {
 	$('.spectrum-item.is-active').removeClass('is-active');
 	$(this).addClass('is-active');
+	alert($(this));
 });
 
 $('.languages-button').on('click', function() {
